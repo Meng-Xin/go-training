09166ecf9772186fac017a7a8aefcd8fb75837dd
Meng-Xin <li997266754@gmail.com>
2023-03-12 15:06:59 +0800

    feat：增添测试用例和dockerfile、docker-compose.yaml

---
+++ example01-container-base-image-updates/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+)
+
+func main() {
+	// http.Handle("/foo", fooHandler)
+
+	http.HandleFunc("/bar", barRes)
+	http.HandleFunc("/ping", pingRes)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func pingRes(w http.ResponseWriter, r *http.Request) {
+	info := "Pong"
+	w.Write([]byte(info))
+}
+
+func barRes(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+}
