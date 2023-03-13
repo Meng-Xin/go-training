4cd1b58f8ff850345ad6883e8c586ed1758415e7
Meng-Xin <li997266754@gmail.com>
2023-03-13 22:13:37 +0800

    fix：修改docker-compose仓库地址

--- example01-container-base-image-updates/main.go
+++ example01-container-base-image-updates/main.go
@@ -12,6 +12,7 @@ func main() {
 
 	http.HandleFunc("/bar", barRes)
 	http.HandleFunc("/ping", pingRes)
+	http.HandleFunc("/watch", watch)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
@@ -23,4 +24,9 @@ func pingRes(w http.ResponseWriter, r *http.Request) {
 
 func barRes(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+}
+
+func watch(w http.ResponseWriter, r *http.Request) {
+	info := "This is Watch"
+	w.Write([]byte(info))
 }
